2ecd3c50cfa9da0b3e2aad59822a5afe93aa59ee
abcdragon <heybrostop@gmail.com>
2017-12-17 18:13:39 +0900

    Handler(+ 약한 참조를 통한 메모리 누수 방지)를 사용한 Sub Thread UI 참조 코드 추가

--- app/src/main/java/uk/hello/org/colordetect/MainActivity.java
+++ app/src/main/java/uk/hello/org/colordetect/MainActivity.java
@@ -3,7 +3,10 @@ package uk.hello.org.colordetect;
 import android.content.Context;
 import android.graphics.Point;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Handler;
+import android.os.Message;
 import android.os.Vibrator;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,17 +15,49 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
+
 public class MainActivity extends AppCompatActivity {
     private Button[] colorBtns = new Button[9];
     private boolean onVibrator = true;
 
     private static final int WRONG = 0xFFDF4949; // 뭔지모를 빨강
     private static final int RIGHT = 0xFF85B14B; // 그리너리
+    private static ActionBar mActionBar;
+
+    // static에서 get~~을 사용할 수 없기 때문에 만든 코드
+    public static ActionBar getmActionBar() {
+        return mActionBar;
+    }
+    // getmActionBar 끝
+
+    // 핸들러 사용시 메모리 누수를 방지하는 코드 (약한 참조를 사용)
+    private static class WrongSignHandler extends Handler {
+        private final WeakReference<MainActivity> mActivity;
+        private WrongSignHandler(MainActivity activity){
+            this.mActivity = new WeakReference<MainActivity>(activity);
+        }
+
+        @Override
+        public void handleMessage(Message msg){
+            MainActivity activity = this.mActivity.get();
+            if(activity != null){
+                if(msg.what == 0){
+                    getmActionBar().setBackgroundDrawable(new ColorDrawable(WRONG));
+                    this.sendEmptyMessageDelayed(1, 500);
+                }
+                else getmActionBar().setBackgroundDrawable(new ColorDrawable(RIGHT));
+            }
+        }
+    }
+    // 핸들러 사용시 메모리 누수를 방지하는 코드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mActionBar = getSupportActionBar(); // getmActionBar 사용을 위한 코드
 
         // 핸드폰의 화면 사이즈 가져오기
         Display display = getWindowManager().getDefaultDisplay();
@@ -53,40 +88,20 @@ public class MainActivity extends AppCompatActivity {
             this.colorBtns[i].setHeight(refSize);
         }
     }
-    // 저장용 + 버튼 크기 재설정
+    // 버튼 저장용 + 버튼 크기 재설정
 
     public void initColor(){}
 
     // 잘못 눌렀을 경우
     public void wrongClick(){
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                getSupportActionBar().setBackgroundDrawable(new ColorDrawable(WRONG));
-                int second = 0;
-                while(second <= 500){
-                    try{
-                        if(onVibrator){
-                            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-                            assert vibrator != null;
-                            vibrator.vibrate(1);
-                            Thread.sleep(1);
-                            second++;
-                        }
-                    }
-                    catch(InterruptedException e) {
-                        String errorMessage = e.getMessage();
-                        Log.e("InterruptedException", errorMessage);
-                    }
-                    catch(NullPointerException e) {
-                        String errorMessage = e.getMessage();
-                        Log.e("NullPointerException", errorMessage);
-                    }
-                }
+        if(onVibrator) {
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            assert vibrator != null;
+            vibrator.vibrate(500);
+        }
 
-                getSupportActionBar().setBackgroundDrawable(new ColorDrawable(RIGHT));
-            }
-        }).start();
+        WrongSignHandler handler = new WrongSignHandler(this);
+        handler.sendEmptyMessage(0);
     }
     // 잘못 눌렀을 경우
 
